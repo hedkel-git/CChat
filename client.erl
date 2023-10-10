cf746e8e6b02dcfdff58f0744d4e150a8d34bf88
hedkel-git <138395061+hedkel-git@users.noreply.github.com>
2023-10-10 19:47:39 +0200

    Add files via upload

--- client.erl
+++ client.erl
@@ -28,26 +28,94 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
 
 % Join channel
 handle(St, {join, Channel}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "join not implemented"}, St} ;
+
+    % Converting Channel to an atom and
+    % checking if it has been created or not
+    ChannelAtom = list_to_atom(Channel),
+
+%    case whereis(ChannelAtom) of
+%        % If channel does not exist, we tell server
+%        % to create it and add ourselves to it.
+%        undefined ->
+%            
+%            %what if server doesn't exist?
+%            Result = genserver:request(St#client_st.server, {create_join, Channel, ChannelAtom, self()}),
+%            % do we need some sort of check for the result of request?
+%            
+%            {reply, Result, St};
+%            %{reply, ok, St};
+%
+%        % If channel does exist, we request to join it directly.
+%        _Pid ->
+%            Result = genserver:request(ChannelAtom, {join, self()}),
+%            {reply, Result, St}
+%    end;
+
+
+    case whereis(ChannelAtom) of
+        % If channel does not exist, we tell server
+        % to create it and add ourselves to it.
+        undefined ->
+            
+            ServerAtom = St#client_st.server,
+            case whereis(ServerAtom) of
+                undefined ->
+                    {reply, {error, server_not_reached, "Server is offline"}, St};
+
+                _Pid ->
+                    case catch(genserver:request(ServerAtom,
+                                {create_join, Channel, ChannelAtom, self()}) ) of
+                        
+                        timeout_error ->
+                            {reply, {error, server_not_reached, "Server is timed out"}, St};
+                        
+                        Result ->
+                            {reply, Result, St}
+                    end
+            end;
+
+        % If channel does exist, we request to join it directly.
+        _Pid ->
+            Result = genserver:request(ChannelAtom, {join, self()}),
+            {reply, Result, St}
+    end;
+
 
 % Leave channel
 handle(St, {leave, Channel}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "leave not implemented"}, St} ;
+
+    ChannelAtom = list_to_atom(Channel),
+
+    case whereis(ChannelAtom) of
+        undefined ->
+            {reply, {error, user_not_joined, "Channel does not exist"}, St};
+
+        _Pid ->
+            {reply, genserver:request(ChannelAtom, {leave, self()}), St}
+    end;
+
+
 
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "message sending not implemented"}, St} ;
+
+    ChannelAtom = list_to_atom(Channel),
+
+    case whereis(ChannelAtom) of
+        undefined ->
+            {reply, {error, user_not_joined, "Channel does not exist"}, St};
+        _Pid ->
+            Result = genserver:request(ChannelAtom, {message_send, self(), St#client_st.nick, Msg}),
+            {reply, Result, St}
+    end;
+
 
 % This case is only relevant for the distinction assignment!
 % Change nick (no check, local only)
 handle(St, {nick, NewNick}) ->
     {reply, ok, St#client_st{nick = NewNick}} ;
+
+
 
 % ---------------------------------------------------------------------------
 % The cases below do not need to be changed...
@@ -57,8 +125,13 @@ handle(St, {nick, NewNick}) ->
 handle(St, whoami) ->
     {reply, St#client_st.nick, St} ;
 
+
 % Incoming message (from channel, to GUI)
 handle(St = #client_st{gui = GUI}, {message_receive, Channel, Nick, Msg}) ->
+    io:fwrite("message recieved~n"),
+    io:format("pid: ~p~n", [self()]),
+
+
     gen_server:call(GUI, {message_receive, Channel, Nick++"> "++Msg}),
     {reply, ok, St} ;
 
--- server.erl
+++ server.erl
@@ -1,5 +1,5 @@
 -module(server).
--export([start/1,stop/1]).
+-export([start/1,stop/1, serverHandle/2, channelHandle/2]).
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
@@ -7,15 +7,99 @@ start(ServerAtom) ->
     % TODO Implement function
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
-    % - Return the process ID
-    not_implemented.
+    % - Return the process id
+    genserver:start(ServerAtom, [], fun server:serverHandle/2).
+
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+
+    genserver:stop(ServerAtom).
+
+% ---------------------------------------------------------------------------
+
+serverHandle(Server, {create_join, ChannelName, ChannelAtom, Client}) ->
+
+    genserver:start(ChannelAtom, createChannel(ChannelName, Client), fun channelHandle/2),
+    {reply, ok, [ChannelAtom | Server]};
 
 
-%only my branch...
+serverHandle(Server, {leave_all_channels, Client}) ->
+    {reply, ok, Server};
+
+
+%leaveAllChannels([], {leave_all_channels, _Client}) ->
+%    ok;
+%leaveAllChannels([Channel | Channels], {leave_all_channels, Client}) ->
+%    genserver:request(Channel, {})
+
+
+
+serverHandle(Server, Data) ->
+    {reply, {error, not_implemented, "Server does not handle this command"}, Server}.
+
+% ---------------------------------------------------------------------------
+-record(channel, {
+    nick,       % name of the channel
+    clients     % list of the clients' PIDs
+}).
+
+createChannel(Nick, Client) ->
+    #channel{nick = Nick, clients = [Client]}.
+
+addClient(Ch = #channel{clients = Clients}, Client) ->
+    Ch#channel{clients = [Client | Clients]}.
+
+removeClient(Ch = #channel{clients = Clients}, Client) ->
+    Ch#channel{clients = lists:delete(Client, Clients)}.
+
+
+channelHandle(Ch = #channel{clients = Clients}, {join, Client}) ->
+    case lists:member(Client, Clients) of
+        true ->
+            {reply, {error, user_already_joined, "Already joined"}, Ch};
+        false ->
+            {reply, ok, addClient(Ch, Client)}
+    end;
+
+
+channelHandle(Ch = #channel{clients = Clients}, {leave, Client}) ->
+    case lists:member(Client, Clients) of
+        true ->
+            {reply, ok, removeClient(Ch, Client)};
+        false ->
+            {reply, {error, user_not_joined, "Cannot leave unassociated channel"}, Ch}
+    end;
+
+
+channelHandle(Ch = #channel{nick = ChannelName, clients = Clients},{message_send, Client, Nick, Msg}) ->
+    case lists:member(Client, Clients) of
+        true ->
+            sendMessageTo(Clients, ChannelName, Client, Nick, Msg),
+            {reply, ok, Ch};
+        false ->
+            {reply, {error, user_not_joined, "Cannot send message in unassociated channel"}, Ch}
+    end;
+
+
+channelHandle(Ch, Data) ->
+    {reply, {error, not_implemented, "Channel does not handle this command"}, Ch}.
+
+
+
+sendMessageTo([], _ChannelName, _Sender, _Nick, _Msg) -> ok;
+
+sendMessageTo([Client | Clients], ChannelName, Sender, Nick, Msg) when Client =/= Sender->
+    % requesting the client to 'show' the message
+    genserver:request(Client, {message_receive, ChannelName, Nick, Msg}),
+
+    sendMessageTo(Clients, ChannelName, Sender, Nick, Msg);
+
+
+sendMessageTo([Client | Clients], ChannelName, Sender, Nick, Msg) ->
+    sendMessageTo(Clients, ChannelName, Sender, Nick, Msg).
+
+
+
+
